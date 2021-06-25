81313f6aea99bdf5f129e27d126ae150ce555133
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-25 16:08:25 +0300

    added dao impls

--- src/main/java/com/NoteTest/note/dao/daoimpl/HashTagDaoImpl.java
+++ src/main/java/com/NoteTest/note/dao/daoimpl/HashTagDaoImpl.java
@@ -1,2 +1,24 @@
-package com.NoteTest.note.dao.daoimpl;public class HashTagDaoImpl {
+package com.NoteTest.note.dao.daoimpl;
+
+import com.NoteTest.note.Entity.HashTagEntity;
+import com.NoteTest.note.dao.HashTagDao;
+import com.NoteTest.note.dao.HibernateUtil;
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Component
+public class HashTagDaoImpl implements HashTagDao {
+    @Override
+    public Set<HashTagEntity> getHashTagsByHashTagId(Set<Long> hashTagIds) {
+        Session session = HibernateUtil.getSession();
+        Set<HashTagEntity> hashTags = new HashSet<>();
+        for (Long hashTagId : hashTagIds) {
+            hashTags.add(session.get(HashTagEntity.class, hashTagId));
+        }
+        session.close();
+        return hashTags;
+    }
 }
--- src/main/java/com/NoteTest/note/dao/daoimpl/NoteDaoImpl.java
+++ src/main/java/com/NoteTest/note/dao/daoimpl/NoteDaoImpl.java
@@ -1,2 +1,64 @@
-package com.NoteTest.note.dao.daoimpl;public class NoteDaoImpl {
+package com.NoteTest.note.dao.daoimpl;
+
+import com.NoteTest.note.Entity.HashTagEntity;
+import com.NoteTest.note.Entity.NoteEntity;
+import com.NoteTest.note.dao.HibernateUtil;
+import com.NoteTest.note.dao.NoteDao;
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class NoteDaoImpl implements NoteDao {
+    @Override
+    public Long addNote(String noteTitle, String noteText, Date date, Set<HashTagEntity> tags) {
+        NoteEntity noteEntity = new NoteEntity();
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        noteEntity.setTitle(noteTitle);
+        noteEntity.setNoteText(noteText);
+        noteEntity.setTags(tags);
+        noteEntity.setCreateDate(date);
+        Long savedId =(Long) session.save(noteEntity);
+        session.getTransaction().commit();
+        session.close();
+        return savedId;
+    }
+
+    @Override
+    public void removeNote(NoteEntity removeNote) {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        session.delete(removeNote);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public void updateNote(NoteEntity updatedNote) {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        session.update(updatedNote);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public List<NoteEntity> getNotes() {
+        Session session = HibernateUtil.getSession();
+        return (List<NoteEntity>) session.createCriteria(NoteEntity.class).list();
+    }
+
+    @Override
+    public NoteEntity getNoteById(Long noteId) {
+        Session session = HibernateUtil.getSession();
+        NoteEntity note = (NoteEntity) session.get(NoteEntity.class, noteId);
+        session.close();
+        return note;
+    }
 }
