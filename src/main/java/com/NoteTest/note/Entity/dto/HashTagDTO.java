dd9ae0dcc74489424af9bd08185ffb379024cba3
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-25 16:10:04 +0300

    added data transfer objects

--- src/main/java/com/NoteTest/note/Entity/dto/HashTagDTO.java
+++ src/main/java/com/NoteTest/note/Entity/dto/HashTagDTO.java
@@ -1,2 +1,44 @@
-package com.NoteTest.note.Entity.dto;public class HashTagDTO {
+package com.NoteTest.note.Entity.dto;
+
+import com.NoteTest.note.Entity.HashTagEntity;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTagDTO {
+    private Long id;
+    private String hashTagName;
+
+    public static Set<HashTagDTO> hashTagSetToHashTagDtoList(Set<HashTagEntity> hashTags){
+        Set<HashTagDTO> hashTagDTOS = new HashSet<>();
+        for (HashTagEntity hashTag : hashTags) {
+            hashTagDTOS.add(hashTagToHashTagDto(hashTag));
+        }
+        return hashTagDTOS;
+    }
+
+    public static HashTagDTO hashTagToHashTagDto(HashTagEntity hashTag){
+        HashTagDTO hashTagDTO = new HashTagDTO();
+        hashTagDTO.setId(hashTag.getId());
+        hashTagDTO.setHashTagName(hashTag.getHashTagName());
+        return hashTagDTO;
+    }
+
+    public HashTagDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHashTagName() {
+        return hashTagName;
+    }
+
+    public void setHashTagName(String hashTagName) {
+        this.hashTagName = hashTagName;
+    }
 }
--- src/main/java/com/NoteTest/note/Entity/dto/NoteDTO.java
+++ src/main/java/com/NoteTest/note/Entity/dto/NoteDTO.java
@@ -1,2 +1,80 @@
-package com.NoteTest.note.Entity.dto;public class NoteDTO {
+package com.NoteTest.note.Entity.dto;
+
+import com.NoteTest.note.Entity.NoteEntity;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public class NoteDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String noteText;
+
+    private Date createDate;
+
+    private Set<HashTagDTO> tags;
+
+
+    public static List<NoteDTO> noteListToNoteDtoList(List<NoteEntity> notes){
+        List<NoteDTO> noteDTOS = new ArrayList<>();
+        for (NoteEntity note : notes) {
+            noteDTOS.add(NoteDTO.NoteToNoteDTO(note));
+        }
+        return noteDTOS;
+    }
+
+    public static NoteDTO NoteToNoteDTO(NoteEntity note){
+        NoteDTO noteDTO = new NoteDTO();
+        noteDTO.setId(note.getId());
+        noteDTO.setNoteText(note.getNoteText());
+        noteDTO.setTitle(note.getTitle());
+        noteDTO.setCreateDate(note.getCreateDate());
+        noteDTO.setTags(HashTagDTO.hashTagSetToHashTagDtoList(note.getTags()));
+        return noteDTO;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Set<HashTagDTO> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<HashTagDTO> tags) {
+        this.tags = tags;
+    }
 }
