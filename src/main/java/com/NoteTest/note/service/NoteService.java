5a628b1011be90bf49c717d8ee7c5471412fbfb0
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-25 16:10:32 +0300

    added note service

--- src/main/java/com/NoteTest/note/service/NoteService.java
+++ src/main/java/com/NoteTest/note/service/NoteService.java
@@ -1,4 +1,18 @@
 package com.NoteTest.note.service;
 
+import com.NoteTest.note.Entity.HashTagEntity;
+import com.NoteTest.note.Entity.NoteEntity;
+import java.util.List;
+import java.util.Set;
+
 public interface NoteService {
+    public void addNote(String noteTitle, String noteText, Set<HashTagEntity> tags);
+    public void removeNote(Long noteId);
+    public void updateNote(Long noteId, String noteTitle, String noteText, Set<HashTagEntity> tags);
+    public List<NoteEntity> getNotes();
+    public List<NoteEntity> getNotesSortByDate();
+    public List<NoteEntity> getNotesThatContainsWord(String Word);
+    public List<NoteEntity> getNotesSortByTags(Set<Long> tagNames);
+
+
 }
--- src/main/java/com/NoteTest/note/service/serviceImpl/NoteServiceImpl.java
+++ src/main/java/com/NoteTest/note/service/serviceImpl/NoteServiceImpl.java
@@ -1,2 +1,82 @@
-package com.NoteTest.note.service.serviceImpl;public class NoteServiceImpl {
+package com.NoteTest.note.service.serviceImpl;
+
+import com.NoteTest.note.Entity.HashTagEntity;
+import com.NoteTest.note.Entity.NoteEntity;
+import com.NoteTest.note.dao.HashTagDao;
+import com.NoteTest.note.dao.NoteDao;
+import com.NoteTest.note.service.NoteService;
+import com.NoteTest.note.util.NoteDateComparator;
+import com.NoteTest.note.util.NoteHashTagSorter;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    private final NoteDao noteDao;
+    private final NoteDateComparator noteDateComparator;
+    private final NoteHashTagSorter noteHashTagSorter;
+    private final HashTagDao hashTagDao;
+
+    public NoteServiceImpl(NoteDao noteDao, NoteDateComparator noteDateComparator, NoteHashTagSorter noteHashTagSorter, HashTagDao hashTagDao) {
+        this.noteDao = noteDao;
+        this.noteDateComparator = noteDateComparator;
+        this.noteHashTagSorter = noteHashTagSorter;
+        this.hashTagDao = hashTagDao;
+    }
+
+    @Override
+    public void addNote(String noteTitle, String noteText, Set<HashTagEntity> tags) {
+        noteDao.addNote(noteTitle,noteText, new Date(),tags);
+    }
+
+    @Override
+    public void removeNote(Long noteId) {
+        NoteEntity removeNote = noteDao.getNoteById(noteId);
+        if(removeNote.getId().equals(noteId)) {//if exists
+            noteDao.removeNote(removeNote);
+        }
+    }
+
+    @Override
+    public void updateNote(Long noteId, String noteTitle, String noteText, Set<HashTagEntity> tags) {
+
+        NoteEntity noteForUpdate = noteDao.getNoteById(noteId);
+
+        if(noteForUpdate.getId().equals(noteId)) {//if exists
+            noteForUpdate.setNoteText(noteText);
+            noteForUpdate.setTitle(noteTitle);
+            noteForUpdate.setTags(tags);
+            noteDao.updateNote(noteForUpdate);
+        }
+    }
+
+    @Override
+    public List<NoteEntity> getNotesSortByDate() {
+        List<NoteEntity> notes = noteDao.getNotes();
+        notes.sort(noteDateComparator);
+        return notes;
+    }
+
+    @Override
+    public List<NoteEntity> getNotes() {
+        List<NoteEntity> notes = noteDao.getNotes();
+        return notes;
+    }
+
+    @Override
+    public List<NoteEntity> getNotesThatContainsWord(String word) {
+        List<NoteEntity> allNotes = noteDao.getNotes();
+        allNotes.removeIf(note -> !(note.getNoteText().contains(word) || note.getTitle().contains(word)));
+        return allNotes;
+    }
+
+    @Override
+    public List<NoteEntity> getNotesSortByTags(Set<Long> tagIds) {
+        List<NoteEntity> allNotes = noteDao.getNotes();
+        Set<HashTagEntity> hashTagByNames = hashTagDao.getHashTagsByHashTagId(tagIds);
+        return noteHashTagSorter.sortNotesByTags(allNotes,hashTagByNames);
+    }
 }
